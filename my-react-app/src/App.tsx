0c23b7134d85f5412542af35f8198044d5c254ef
= <=>
2024-03-22 13:16:47 +0200

    Add my info to App.tsx

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,25 +1,17 @@
+// @ts-ignore
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+const App: React.FC = () => {
+    return (
+      <center>
+      <div>
+        <h1>Bejenar Veaceslav</h1>
+        <h2>CR-222</h2>
+        <h3>Laboratorul 2</h3>
+        <h4>Tehnologii WEB</h4>
+      </div>
+      </center>
   );
 }
 
